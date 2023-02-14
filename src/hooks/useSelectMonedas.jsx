29d3706a54e1abf391bb4222eb5f78df083b5aa3
jhechus <jess.osm1412@gmail.com>
2023-02-13 19:28:00 -0600

    primer hook

---
+++ src/hooks/useSelectMonedas.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function useSelectMonedas() {
+  
+    const SelectMonedas = () => {
+        console.log('Desde hook')
+    }
+
+    return [ SelectMonedas]
+}
+
+export default useSelectMonedas
